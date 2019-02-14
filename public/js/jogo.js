72a8135bd068415c92f35c0af654dcc8634b1fc1
Gustavo Colombelli Alessio <gustavocolombelli@gmail.com>
2019-02-14 10:12:37 -0200

    reconfiguração de diretórios e implementação de jogador parado

---
+++ public/js/jogo.js
@@ -0,0 +1,95 @@
+const socket = io();
+
+let pos = {x: 0, y: 0};
+let posAtual = {x: 0, y: 0};
+let posAnterior = {x: 0, y: 0};
+
+var up = false,
+    right = false,
+    down = false,
+    left = false,
+    x = window.innerWidth / 2 - 130 / 2;
+y = window.innerHeight / 2 - 130 / 2;
+posAtual.x = window.innerWidth / 2 - 130 / 2;
+posAtual.y = window.innerHeight / 2 - 130 / 2;
+
+socket.on('connect', function () {
+    $("#msgs").append("conectado com o id: " + socket.id);
+    socket.emit('msg', 'estou conectado: ' + socket.id);
+});
+
+socket.on('msg', function (msg) {
+    $("#msgs").append('<br>' + msg);
+    console.log('outro usuario: ', msg);
+    console.log(msg.x);
+});
+
+
+document.addEventListener('keydown', press);
+
+function press(e) {
+    if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */) {
+        up = true
+    }
+    if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */) {
+        right = true
+    }
+    if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */) {
+        down = true
+    }
+    if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */) {
+        left = true
+    }
+}
+
+document.addEventListener('keyup', release);
+
+function release(e) {
+    if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */) {
+        up = false
+    }
+    if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */) {
+        right = false
+    }
+    if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */) {
+        down = false
+    }
+    if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */) {
+        left = false
+    }
+}
+
+function gameLoop() {
+    var div = document.querySelector('div');
+    console.log('POS ANTERIOR: ', posAnterior);
+    if (up) {
+        posAtual.y = posAtual.y - 10
+    }
+    if (right) {
+        posAtual.x = posAtual.x + 10
+    }
+    if (down) {
+        posAtual.y = posAtual.y + 10
+    }
+    if (left) {
+        posAtual.x = posAtual.x - 10
+    }
+    div.style.left = posAtual.x + 'px';
+    div.style.top = posAtual.y + 'px';
+    window.requestAnimationFrame(gameLoop);
+    //verificar posição anterior para não repitir envios iguais
+
+    //Emitir ao servidor a posição atual do jogador
+    if (posAnterior && (posAtual.x !== posAnterior.x || posAtual.y !== posAnterior.y)) {
+        socket.emit('msg', posAtual);
+    }
+
+    posAnterior.x = posAtual.x;
+    posAnterior.y = posAtual.y;
+    console.log('POS ATUAL: ', posAtual);
+}
+
+window.requestAnimationFrame(gameLoop);
+
+// console.log($(window).height());
+// console.log($(window).width());
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-const app = require('express')();
-const http = require('http').createServer(app);
+var express = require('express');
+var app = express();
 
+var jogadores = {};
+
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res)=>{
-	res.sendFile(__dirname+'/index.html');
+const porta = 80;
+const ip = "10.35.12.143";
+
+let contadorMovimentos=0;
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+
 });
 
-io.on('connection', (socket)=>{
-	console.log('novo cliente conectado', socket.id);
-	socket.on('msg', (msg)=> {
+io.on('connection', (socket) => {
+    console.log('novo cliente conectado', socket.id);
+
+    socket.on('msg', (msg) => {
+        console.log('---', contadorMovimentos++);
         console.log(msg, socket.id);
         msg.id = socket.id;
-		socket.broadcast.emit('msg', msg);
-	})
+        socket.broadcast.emit('msg', msg);
+    });
+
 });
 
-http.listen(3000, function () {
-	console.log('Ouvindo na porta 3000')
+http.listen(porta, ip, function () {
+    console.log('Ouvindo na porta:' + porta)
 });
